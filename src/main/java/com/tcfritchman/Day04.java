7c50d68469ffbece9c9cca9eadc8dfae17680bcc
tcfritchman <tcfritchman@gmail.com>
2024-12-04 20:00:14 -0800

    day 3

---
+++ src/main/java/com/tcfritchman/Day04.java
@@ -0,0 +1,23 @@
+package com.tcfritchman;
+
+public class Day04 {
+
+    static String exampleInput = """
+                """;
+
+    static String realInput = """
+               """;
+
+    public static void main(String[] args) {
+        System.out.println("Part 1 --- example: " + part1(exampleInput) + " --- real: " + part1(realInput));
+        System.out.println("Part 2 --- example: " + part2(exampleInput) + " --- real: " + part2(realInput));
+    }
+
+    private static Object part1(String input) {
+        return null;
+    }
+
+    private static Object part2(String input) {
+        return null;
+    }
+}
