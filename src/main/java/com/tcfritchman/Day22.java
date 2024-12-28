d5304fd79c8f70fcb20c6fa390504f8ec4f34bba
tcfritchman <tcfritchman@gmail.com>
2024-12-28 10:51:54 -0800

    add remaining days

---
+++ src/main/java/com/tcfritchman/Day22.java
@@ -0,0 +1,25 @@
+package com.tcfritchman;
+
+public class Day22 {
+
+    static String exampleInput = """
+            """;
+
+    static String realInput = """
+            """;
+
+    public static void main(String[] args) {
+        System.out.println("Part 1 --- example: " + part1(exampleInput));
+        System.out.println("Part 1 --- real: " + part1(realInput));
+        System.out.println("Part 2 --- example: " + part2(exampleInput));
+        System.out.println("Part 2 --- real: " + part2(realInput));
+    }
+
+    private static Object part1(String input) {
+        return null;
+    }
+
+    private static Object part2(String input) {
+        return null;
+    }
+}
